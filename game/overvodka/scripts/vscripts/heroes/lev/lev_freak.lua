85701448ee592a6d3722ec3c7eff8f3e0441a409
worstsup <worstsup@vk.com>
2025-03-18 10:44:26 +0300

    поправил эффекты

--- game/overvodka/scripts/vscripts/heroes/lev/lev_freak.lua
+++ game/overvodka/scripts/vscripts/heroes/lev/lev_freak.lua
@@ -5,7 +5,8 @@ lev_freak = class({})
 
 function lev_freak:Precache(context)
     PrecacheResource("soundfile", "soundevents/freak.vsndevts", context)
-    PrecacheResource("particle", "particles/units/heroes/hero_nyx_assassin/nyx_assassin_spiked_carapace.vpcf", context)
+    PrecacheResource("particle", "particles/econ/items/nyx_assassin/nyx_ti9_immortal/nyx_ti9_carapace.vpcf", context)
+    PrecacheResource("particle", "particles/econ/items/ancient_apparition/aa_2021_immortal/aa_2021_immortal_chilling_projectile_hit.vpcf", context)
 end
 
 function lev_freak:OnSpellStart()
@@ -34,7 +35,7 @@ function modifier_lev_freak:DeclareFunctions()
 end
 
 function modifier_lev_freak:GetEffectName()
-    return "particles/units/heroes/hero_nyx_assassin/nyx_assassin_spiked_carapace.vpcf"
+    return "particles/econ/items/nyx_assassin/nyx_ti9_immortal/nyx_ti9_carapace.vpcf"
 end
 
 function modifier_lev_freak:GetEffectAttachType()
@@ -50,5 +51,9 @@ function modifier_lev_freak:OnTakeDamage(event)
     if event.unit == parent and attacker:GetTeamNumber() ~= parent:GetTeamNumber() and not parent.carapaced_units[ attacker:entindex() ] then
         attacker:AddNewModifier(parent, self:GetAbility(), "modifier_generic_stunned_lua", { duration = self.stun_duration })
         parent.carapaced_units[ attacker:entindex() ] = attacker
+        local particle = ParticleManager:CreateParticle("particles/econ/items/ancient_apparition/aa_2021_immortal/aa_2021_immortal_chilling_projectile_hit.vpcf", PATTACH_ABSORIGIN_FOLLOW, attacker)
+        ParticleManager:SetParticleControlEnt(particle, 1, attacker, PATTACH_POINT_FOLLOW, "attach_hitloc", attacker:GetAbsOrigin(), true)
+        ParticleManager:SetParticleControlEnt(particle, 3, attacker, PATTACH_POINT_FOLLOW, "attach_hitloc", attacker:GetAbsOrigin(), true)
+        ParticleManager:ReleaseParticleIndex(particle)
     end
 end
