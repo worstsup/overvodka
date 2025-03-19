12859fa79a58aeae06737ba95c3be2a73bb44334
worstsup <worstsup@vk.com>
2025-03-19 19:44:46 +0300

    почистил от говна

--- game/overvodka/scripts/vscripts/acid_spray.lua
+++
@@ -1,6 +0,0 @@
-function AcidSpraySound( event )
-	local target = event.target
-	local ability = event.ability
-	local duration = ability:GetLevelSpecialValueFor( "duration", ability:GetLevel() - 1 )
-	target:EmitSound("blue")
-end
