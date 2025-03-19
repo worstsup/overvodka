296187776fca3a6f763aecc179180da6a8d727db
worstsup <worstsup@vk.com>
2025-03-19 17:55:36 +0300

    azazin e, r

---
+++ game/overvodka/scripts/vscripts/heroes/azazin/azazin_e.lua
@@ -0,0 +1,224 @@
+LinkLuaModifier("modifier_azazin_e", "heroes/azazin/azazin_e", LUA_MODIFIER_MOTION_NONE)
+LinkLuaModifier("modifier_azazin_e_caster", "heroes/azazin/azazin_e", LUA_MODIFIER_MOTION_NONE)
+LinkLuaModifier("modifier_azazin_e_enemy", "heroes/azazin/azazin_e", LUA_MODIFIER_MOTION_NONE)
+LinkLuaModifier("modifier_azazin_e_debuff", "heroes/azazin/azazin_e", LUA_MODIFIER_MOTION_NONE)
+LinkLuaModifier("modifier_generic_stunned_lua", "modifier_generic_stunned_lua", LUA_MODIFIER_MOTION_NONE)
+
+azazin_e = class({})
+k = 0
+function azazin_e:Precache(context)
+    PrecacheResource("particle", "particles/azazin_e_stack.vpcf", context)
+    PrecacheResource("particle", "particles/units/heroes/hero_monkey_king/monkey_king_quad_tap_start.vpcf", context)
+    PrecacheResource("particle", "particles/units/heroes/hero_monkey_king/monkey_king_quad_tap_overhead.vpcf", context)
+    PrecacheResource("particle", "particles/generic_gameplay/generic_lifesteal.vpcf", context)
+    PrecacheResource("particle", "particles/azazin_e_debuff.vpcf", context)
+    PrecacheResource("particle", "particles/azazin_e_exp.vpcf", context)
+    PrecacheResource("soundfile", "soundevents/azazin_e_1.vsndevts", context)
+    PrecacheResource("soundfile", "soundevents/azazin_e_2.vsndevts", context)
+    PrecacheResource("soundfile", "soundevents/azazin_e_3.vsndevts", context)
+    PrecacheResource("soundfile", "soundevents/azazin_e_4.vsndevts", context)
+end
+
+function azazin_e:GetIntrinsicModifierName() 
+    return "modifier_azazin_e"
+end
+
+modifier_azazin_e = class({})
+
+function modifier_azazin_e:IsHidden() 
+    return true
+end
+function modifier_azazin_e:IsPurgable() 
+    return false 
+end
+
+function modifier_azazin_e:DeclareFunctions()
+    return {
+        MODIFIER_EVENT_ON_ATTACK_LANDED,
+    }
+end
+
+function modifier_azazin_e:OnCreated()
+    if not IsServer() then return end
+end
+
+function modifier_azazin_e:OnAttackLanded(params)
+    if not IsServer() then return end
+    if params.attacker ~= self:GetParent() then return end
+    if params.target:IsWard() then return end
+    if params.no_attack_cooldown then return end
+    if self:GetParent():PassivesDisabled() then return end
+    if self:GetParent():IsIllusion() then return end
+
+    local target = params.target
+    local ability = self:GetAbility()
+    local required_hits = ability:GetSpecialValueFor("required_hits")
+    local counter_duration = ability:GetSpecialValueFor("counter_duration")
+    local effect_duration = ability:GetSpecialValueFor("effect_duration")
+    local stun_duration = ability:GetSpecialValueFor("stun_duration")
+    if target:HasModifier("modifier_azazin_e_enemy") then
+        local enemyMod = target:FindModifierByName("modifier_azazin_e_enemy")
+        enemyMod:IncrementStackCount()
+        enemyMod:SetDuration(counter_duration, true)
+        if enemyMod:GetStackCount() >= required_hits then
+            enemyMod:Destroy()
+            self:GetParent():AddNewModifier(self:GetParent(), ability, "modifier_azazin_e_caster", { duration = counter_duration })
+            target:AddNewModifier(self:GetParent(), ability, "modifier_generic_stunned_lua", { duration = stun_duration })
+            target:AddNewModifier(self:GetParent(), ability, "modifier_azazin_e_debuff", { duration = effect_duration })
+            if k == 0 then
+                EmitSoundOn("azazin_e_1", target)
+                k = 1
+            elseif k == 1 then
+                EmitSoundOn("azazin_e_2", target)
+                k = 2
+            elseif k == 2 then
+                EmitSoundOn("azazin_e_3", target)
+                k = 3
+            elseif k == 3 then
+                EmitSoundOn("azazin_e_4", target)
+                k = 0
+            end
+            local effect_cast = ParticleManager:CreateParticle("particles/azazin_e_exp.vpcf", PATTACH_ABSORIGIN_FOLLOW, target)
+            ParticleManager:SetParticleControlEnt(
+                effect_cast,
+                3,
+                target,
+                PATTACH_POINT_FOLLOW,
+                "attach_hitloc",
+                Vector(0,0,0),
+                true
+            )
+            ParticleManager:ReleaseParticleIndex( effect_cast )
+        end
+    else
+        target:AddNewModifier(self:GetParent(), ability, "modifier_azazin_e_enemy", { duration = counter_duration })
+        local enemyMod = target:FindModifierByName("modifier_azazin_e_enemy")
+        if enemyMod then
+            enemyMod:SetStackCount(1)
+        end
+    end
+end
+
+modifier_azazin_e_enemy = class({})
+
+function modifier_azazin_e_enemy:IsHidden() 
+    return false 
+end
+
+function modifier_azazin_e_enemy:IsDebuff() 
+    return true 
+end
+
+function modifier_azazin_e_enemy:IsPurgable() 
+    return false 
+end
+
+function modifier_azazin_e_enemy:RemoveOnDeath() 
+    return true 
+end
+
+function modifier_azazin_e_enemy:OnCreated(kv)
+    if not IsServer() then return end
+    self:SetStackCount(1)
+    self.particle = ParticleManager:CreateParticle("particles/azazin_e_stack.vpcf", PATTACH_OVERHEAD_FOLLOW, self:GetParent())
+    ParticleManager:SetParticleControl(self.particle, 1, Vector(0, self:GetStackCount(), 0))
+    self:AddParticle(self.particle, false, false, -1, false, false)
+end
+
+function modifier_azazin_e_enemy:OnStackCountChanged(previous_stacks)
+    if not IsServer() then return end
+    if not self.particle then return end
+    ParticleManager:SetParticleControl(self.particle, 1, Vector(0, self:GetStackCount(), 0))
+end
+
+function modifier_azazin_e_enemy:OnRefresh(kv)
+    if not IsServer() then return end
+    self:IncrementStackCount()
+end
+
+modifier_azazin_e_caster = class({})
+
+function modifier_azazin_e_caster:IsPurgable() 
+    return false 
+end
+
+function modifier_azazin_e_caster:OnCreated()
+    if not IsServer() then return end
+    self.particle1 = ParticleManager:CreateParticle("particles/units/heroes/hero_monkey_king/monkey_king_quad_tap_start.vpcf", PATTACH_ABSORIGIN_FOLLOW, self:GetParent())
+    ParticleManager:SetParticleControl(self.particle1, 0, self:GetParent():GetAbsOrigin())
+    self.particle = ParticleManager:CreateParticle("particles/units/heroes/hero_monkey_king/monkey_king_quad_tap_overhead.vpcf", PATTACH_OVERHEAD_FOLLOW, self:GetParent())
+    ParticleManager:SetParticleControl(self.particle, 0, self:GetParent():GetAbsOrigin())
+    self:AddParticle(self.particle1, false, false, -1, false, false)
+    self:AddParticle(self.particle, false, false, -1, false, true)
+    self.record = nil
+end
+
+function modifier_azazin_e_caster:DeclareFunctions()
+    return {
+        MODIFIER_EVENT_ON_TAKEDAMAGE,
+        MODIFIER_PROPERTY_PROCATTACK_BONUS_DAMAGE_PHYSICAL,
+    }
+end
+
+function modifier_azazin_e_caster:GetModifierProcAttack_BonusDamage_Physical(params)
+    if not IsServer() then return end
+    if params.target:IsWard() then return end
+    if self:GetParent():IsIllusion() then return end
+    local damage = self:GetAbility():GetSpecialValueFor("bonus_damage")
+    self.record = params.record
+    return damage
+end
+
+function modifier_azazin_e_caster:OnTakeDamage(params)
+    if not IsServer() then return end
+    if self:GetParent() ~= params.attacker then return end
+    if self:GetParent() == params.unit then return end
+    if params.unit:IsBuilding() then return end
+    if params.unit:IsWard() then return end
+    if (params.inflictor == nil or params.inflictor:GetAbilityName() == "item_revenants_brooch") and not self:GetParent():IsIllusion() and bit.band(params.damage_flags, DOTA_DAMAGE_FLAG_REFLECTION) ~= DOTA_DAMAGE_FLAG_REFLECTION then 
+        if params.record ~= self.record then return end
+        local heal = self:GetAbility():GetSpecialValueFor("lifesteal") / 100 * params.damage
+        self:GetParent():Heal(heal, self:GetAbility())
+        local effect_cast = ParticleManager:CreateParticle("particles/generic_gameplay/generic_lifesteal.vpcf", PATTACH_ABSORIGIN_FOLLOW, params.attacker)
+        ParticleManager:ReleaseParticleIndex(effect_cast)
+        self:Destroy()
+    end
+end
+
+modifier_azazin_e_debuff = class({})
+function modifier_azazin_e_debuff:IsDebuff() return true end
+function modifier_azazin_e_debuff:IsPurgable() return true end
+
+function modifier_azazin_e_debuff:OnCreated()
+    if not IsServer() then return end
+    self.interval = 0.5
+    self:StartIntervalThink(self.interval)
+end
+
+function modifier_azazin_e_debuff:OnIntervalThink()
+    if not IsServer() then return end
+    local damage = self:GetAbility():GetSpecialValueFor("tick_damage") * self.interval
+    local damageTable = {
+        victim = self:GetParent(),
+        attacker = self:GetCaster(),
+        damage = damage,
+        damage_type = self:GetAbility():GetAbilityDamageType(),
+        ability = self:GetAbility(),
+    }
+    ApplyDamage(damageTable)
+end
+
+function modifier_azazin_e_debuff:DeclareFunctions()
+    return {
+        MODIFIER_PROPERTY_PHYSICAL_ARMOR_BONUS,
+    }
+end
+function modifier_azazin_e_debuff:GetModifierPhysicalArmorBonus()
+    return self:GetAbility():GetSpecialValueFor("armor_reduction")
+end
+function modifier_azazin_e_debuff:GetEffectName()
+    return "particles/azazin_e_debuff.vpcf"
+end
+function modifier_azazin_e_debuff:GetEffectAttachType()
+    return PATTACH_ABSORIGIN_FOLLOW
+end
---
+++ game/overvodka/scripts/vscripts/heroes/azazin/azazin_r.lua
@@ -0,0 +1,189 @@
+LinkLuaModifier( "modifier_azazin_r", "heroes/azazin/azazin_r", LUA_MODIFIER_MOTION_NONE )
+LinkLuaModifier( "modifier_azazin_r_debuff", "heroes/azazin/azazin_r", LUA_MODIFIER_MOTION_NONE )
+
+azazin_r = class({})
+k = 0
+function azazin_r:Precache(context)
+    PrecacheResource( "particle", "particles/azazin_r_ambient.vpcf", context )
+    PrecacheResource( "particle", "particles/azazin_r_radius.vpcf", context )
+    PrecacheResource( "particle", "particles/azazin_r_debuff.vpcf", context )
+    PrecacheResource( "soundfile", "soundevents/game_sounds_heroes/game_sounds_dark_willow.vsndevts", context )
+    PrecacheResource( "soundfile", "soundevents/azazin_r_1.vsndevts", context )
+    PrecacheResource( "soundfile", "soundevents/azazin_r_2.vsndevts", context )
+    PrecacheResource( "soundfile", "soundevents/azazin_r.vsndevts", context )
+    PrecacheResource( "model", "models/azazin/azazin_girl.vmdl", context )
+end
+
+function azazin_r:GetCooldown(level)
+    return self.BaseClass.GetCooldown( self, level )
+end
+function azazin_r:GetManaCost(level)
+    return self.BaseClass.GetManaCost(self, level)
+end
+
+function azazin_r:GetCastRange(location, target)
+    return self:GetSpecialValueFor( "radius" )
+end
+
+function azazin_r:OnToggle()
+	local caster = self:GetCaster()
+	local toggle = self:GetToggleState()
+
+	if toggle then
+		self.modifier = caster:AddNewModifier( caster, self, "modifier_azazin_r", {})
+		self:EndCooldown()
+	else
+		if self.modifier and not self.modifier:IsNull() then
+			self.modifier:Destroy()
+		end
+		self.modifier = nil
+		self:UseResources(false, false, false, true)
+	end
+end
+
+modifier_azazin_r = class({})
+
+function modifier_azazin_r:GetAttributes()
+	return MODIFIER_ATTRIBUTE_PERMANENT 
+end
+
+function modifier_azazin_r:IsPurgable()
+	return false
+end
+function modifier_azazin_r:IsAura()
+    return true
+end
+
+function modifier_azazin_r:GetAuraRadius()
+    return self:GetAbility():GetSpecialValueFor("radius")
+end
+
+function modifier_azazin_r:GetModifierAura()
+    return "modifier_azazin_r_debuff"
+end
+
+function modifier_azazin_r:GetAuraSearchTeam()
+    return DOTA_UNIT_TARGET_TEAM_ENEMY
+end
+
+function modifier_azazin_r:GetAuraSearchType()
+    return DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_BASIC
+end
+function modifier_azazin_r:OnCreated( kv )
+	if not IsServer() then return end
+	local damage = self:GetAbility():GetSpecialValueFor( "damage" )
+	self.radius = self:GetAbility():GetSpecialValueFor( "radius" )
+	self.manacost = self:GetAbility():GetSpecialValueFor( "mana_cost_per_second" )
+    self.gold_per_second = self:GetAbility():GetSpecialValueFor( "gold_per_second" )
+    local particle = ParticleManager:CreateParticle("particles/azazin_r_radius.vpcf", PATTACH_ABSORIGIN_FOLLOW, self:GetParent())
+    ParticleManager:SetParticleControl(particle, 1, Vector(self.radius + 25, self.radius + 25, self.radius + 25))
+    self:AddParticle(particle, false, false, -1, false, false)
+	self.damageTable = 
+	{
+		attacker = self:GetParent(),
+		damage = damage,
+		damage_type = self:GetAbility():GetAbilityDamageType(),
+		ability = self:GetAbility(),
+	}
+	local interval = 1
+	self.parent = self:GetParent()
+	self:Burn()
+	self:StartIntervalThink( interval )
+	if k == 0 then
+        EmitSoundOnLocationWithCaster(self.parent:GetAbsOrigin(),"azazin_r_1", self.parent)
+        k = 1
+    else
+        EmitSoundOnLocationWithCaster(self.parent:GetAbsOrigin(),"azazin_r_2", self.parent)
+        k = 0
+    end
+    EmitSoundOn("azazin_r", self.parent)
+end
+
+function modifier_azazin_r:DeclareFunctions()
+    local funcs = 
+    {
+        MODIFIER_PROPERTY_MODEL_CHANGE,
+        MODIFIER_PROPERTY_PROJECTILE_NAME,
+        MODIFIER_PROPERTY_TRANSLATE_ATTACK_SOUND
+    }
+    return funcs
+end
+
+function modifier_azazin_r:GetModifierProjectileName()
+    return "particles/units/heroes/hero_dark_willow/dark_willow_base_attack.vpcf"
+end
+
+function modifier_azazin_r:GetModifierModelChange()
+    return "models/azazin/azazin_girl.vmdl"
+end
+
+function modifier_azazin_r:GetAttackSound()
+	return "Hero_DarkWillow.Attack"
+end
+function modifier_azazin_r:OnDestroy()
+	if not IsServer() then return end
+	StopSoundOn("azazin_r", self.parent)
+end
+
+function modifier_azazin_r:OnIntervalThink()
+	local mana = self.parent:GetMana()
+	if mana < self.manacost then
+		if self:GetAbility():GetToggleState() then
+			self:GetAbility():ToggleAbility()
+		end
+		return
+	end
+	self:Burn()
+end
+
+function modifier_azazin_r:Burn()
+	self.parent:SpendMana( self.manacost, self:GetAbility() )
+
+	local enemies = FindUnitsInRadius( self.parent:GetTeamNumber(), self.parent:GetOrigin(), nil, self.radius, DOTA_UNIT_TARGET_TEAM_ENEMY, DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_BASIC, 0, 0, false)
+
+	for _,enemy in pairs(enemies) do
+		self.damageTable.victim = enemy
+		ApplyDamage( self.damageTable )
+        PlayerResource:SpendGold(enemy:GetPlayerID(), self.gold_per_second, 4)
+        self.parent:ModifyGold(self.gold_per_second, false, 0)
+	end
+end
+
+function modifier_azazin_r:GetEffectName()
+	return "particles/azazin_r_ambient.vpcf"
+end
+
+function modifier_azazin_r:GetEffectAttachType()
+	return PATTACH_ABSORIGIN_FOLLOW
+end
+
+modifier_azazin_r_debuff = class({})
+
+function modifier_azazin_r_debuff:IsPurgable()
+    return false
+end
+
+function modifier_azazin_r_debuff:OnCreated()
+    if not IsServer() then return end
+end
+
+function modifier_azazin_r_debuff:DeclareFunctions()
+    return {MODIFIER_PROPERTY_MOVESPEED_BONUS_PERCENTAGE,
+            MODIFIER_PROPERTY_ATTACKSPEED_BONUS_CONSTANT}
+end
+
+function modifier_azazin_r_debuff:GetEffectName()
+    return "particles/azazin_r_debuff.vpcf"
+end
+
+function modifier_azazin_r_debuff:GetEffectAttachType()
+    return PATTACH_OVERHEAD_FOLLOW
+end
+
+function modifier_azazin_r_debuff:GetModifierMoveSpeedBonus_Percentage()
+    return self:GetAbility():GetSpecialValueFor("movespeed_slow")
+end
+
+function modifier_azazin_r_debuff:GetModifierAttackSpeedBonus_Constant()
+    return self:GetAbility():GetSpecialValueFor("attack_speed_slow")
+end
