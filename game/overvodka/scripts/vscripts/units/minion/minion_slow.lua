30c658d078a3ca57328d31a4bf528b3ff8b28782
worstsup <worstsup@vk.com>
2025-03-26 23:53:14 +0300

    minions

--- game/overvodka/scripts/vscripts/units/minion/minion_slow.lua
+++ game/overvodka/scripts/vscripts/units/minion/minion_slow.lua
@@ -3,11 +3,6 @@ LinkLuaModifier("modifier_minion_slow_debuff", "units/minion/minion_slow", LUA_M
 
 minion_slow = class({})
 
-function minion_slow:Precache(context)
-    PrecacheResource("particle", "particles/generic_gameplay/generic_slowed_cold.vpcf", context)
-    PrecacheResource("soundfile", "particles/units/heroes/hero_monkey_king/monkey_king_taunt_bananas.vpcf", context)
-end
-
 function minion_slow:GetIntrinsicModifierName()
     return "modifier_minion_slow_passive"
 end
@@ -19,10 +14,9 @@ function modifier_minion_slow_passive:IsPurgable() return false end
 function modifier_minion_slow_passive:RemoveOnDeath() return false end
 
 function modifier_minion_slow_passive:DeclareFunctions()
-    local funcs = {
+    return {
         MODIFIER_EVENT_ON_ATTACK_LANDED,
     }
-    return funcs
 end
 
 function modifier_minion_slow_passive:OnAttackLanded(params)
@@ -30,26 +24,9 @@ function modifier_minion_slow_passive:OnAttackLanded(params)
     if self:GetParent():PassivesDisabled() then return end
     if params.attacker ~= self:GetParent() then return end
     if params.target:IsBuilding() then return end
-
     local target = params.target
     local ability = self:GetAbility()
-    local slow_value = ability:GetSpecialValueFor("slow")
-    local duration = ability:GetSpecialValueFor("duration")
-    local max_stacks = ability:GetSpecialValueFor("max_stacks")
-    local modifierName = "modifier_minion_slow_debuff"
-    local debuff = target:FindModifierByNameAndCaster(modifierName, self:GetParent())
-    if debuff then
-        if debuff:GetStackCount() < max_stacks then
-            debuff:IncrementStackCount()
-        end
-        debuff:SetDuration(duration, true)
-    else
-        target:AddNewModifier(self:GetParent(), ability, modifierName, {duration = duration})
-        debuff = target:FindModifierByNameAndCaster(modifierName, self:GetParent())
-        if debuff then
-            debuff:SetStackCount(1)
-        end
-    end
+    target:AddNewModifier(self:GetParent(), ability, "modifier_minion_slow_debuff", {duration = ability:GetSpecialValueFor("duration")})
     local particle = ParticleManager:CreateParticle("particles/units/heroes/hero_monkey_king/monkey_king_taunt_bananas.vpcf", PATTACH_ABSORIGIN_FOLLOW, target)
     ParticleManager:SetParticleControl(particle, 0, target:GetAbsOrigin())
     ParticleManager:ReleaseParticleIndex(particle)
@@ -63,10 +40,9 @@ function modifier_minion_slow_debuff:IsPurgable() return true end
 function modifier_minion_slow_debuff:RemoveOnDeath() return true end
 
 function modifier_minion_slow_debuff:DeclareFunctions()
-    local funcs = {
+    return {
         MODIFIER_PROPERTY_MOVESPEED_BONUS_PERCENTAGE,
     }
-    return funcs
 end
 
 function modifier_minion_slow_debuff:GetModifierMoveSpeedBonus_Percentage()
@@ -86,4 +62,27 @@ end
 
 function modifier_minion_slow_debuff:GetTexture()
     return "minion_slow"
+end
+
+function modifier_minion_slow_debuff:OnCreated(kv)
+    if not IsServer() then return end
+    self:SetStackCount(1)
+    self:SetDuration(self:GetAbility():GetSpecialValueFor("duration"), true)
+end
+
+function modifier_minion_slow_debuff:OnRefresh(kv)
+    if not IsServer() then return end
+    local ability = self:GetAbility()
+    local max_stacks = ability:GetSpecialValueFor("max_stacks")
+    if self:GetStackCount() < max_stacks then
+        self:IncrementStackCount()
+    end
+    self:SetDuration(ability:GetSpecialValueFor("duration"), true)
+    if self:GetStackCount() >= max_stacks then
+        local attacker = self:GetParent()
+        if not attacker.minion_laugh_last_time or GameRules:GetGameTime() >= attacker.minion_laugh_last_time + 10 then
+            attacker:EmitSound("minion_laugh")
+            attacker.minion_laugh_last_time = GameRules:GetGameTime()
+        end
+    end
 end
