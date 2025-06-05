37ab55c9a58f6599082259f53fdb938879c908c5
Leny24 <garipoffbog@gmail.com>
2025-06-05 14:28:13 +0300

    автопрекеш партиклов для addon_game_mode

--- game/overvodka/scripts/vscripts/addon_game_mode.lua
+++ game/overvodka/scripts/vscripts/addon_game_mode.lua
@@ -828,3 +828,45 @@ function COverthrowGameMode:AssignTeams()
 		GameRules:BotPopulate()
 	end
 end
+
+if IsServer() then
+    -- Effects created with the projectile manager seem to be initiated properly without precaching.
+    -- Note: Might also work with modifier's GetEffectName() function.
+    local CreateEffectFixProjectile = function(sEffectName)
+        local hExplosion =  {   
+                                Ability             = nil,
+                                EffectName          = sEffectName,
+                                vSpawnOrigin        = Vector(0, 0, 0),
+                                fDistance           = 0,
+                                fStartRadius        = 0,
+                                fEndRadius          = 0,
+                                Source              = nil,
+                                bHasFrontalCone     = false,
+                                bReplaceExisting    = false,
+                                iUnitTargetTeam     = 0,
+                                iUnitTargetFlags    = 0,
+                                iUnitTargetType     = 0,
+                                fExpireTime         = GameRules:GetGameTime() + FrameTime(),
+                                --bDeleteOnHit        = false,
+                                vVelocity           = 0,
+                                bProvidesVision     = false,
+                                iVisionRadius       = 0,
+                                iVisionTeamNumber   = 0,
+                                --ExtraData             = {}
+                            }
+
+        return ProjectileManager:CreateLinearProjectile(hExplosion)
+    end
+    --==============================================================================================================--
+    local VALVE_CScriptParticleManager_CreateParticle = CScriptParticleManager.CreateParticle
+    CScriptParticleManager.CreateParticle = function(self, sParticleName, iParticleAttachment, hOwner)
+        local iProjectile = CreateEffectFixProjectile(sParticleName)
+        if iProjectile and ProjectileManager:IsValidProjectile(iProjectile) then
+            ProjectileManager:DestroyLinearProjectile(iProjectile)
+        end
+        
+        --print("Server: Overriding Gaben Particle Function...")
+
+        return VALVE_CScriptParticleManager_CreateParticle(self, sParticleName, iParticleAttachment, hOwner)
+    end
+end
