e2bd0fab59fe415fff9754dd3c7e9c13fa8edac7
Mikhail Russov <worstsup@vk.com>
2026-02-18 20:19:10 +0300

    Fix regen modifier rebind when upgrading charik into govno

--- game/overvodka/scripts/vscripts/items/govno.lua
+++ game/overvodka/scripts/vscripts/items/govno.lua
@@ -32,7 +32,16 @@ function modifier_govno:OnCreated()
 	local parent = self:GetParent()
 	if not parent or parent:IsNull() then return end
 
-    if not parent:HasModifier("modifier_item_charik_new_regen") then
+	local regen = parent:FindModifierByName("modifier_item_charik_new_regen")
+	if regen and not regen:IsNull() then
+		local regen_ability = regen:GetAbility()
+		if not regen_ability or regen_ability:IsNull() then
+			regen:Destroy()
+			regen = nil
+		end
+	end
+
+	if not regen then
 		parent:AddNewModifier(parent, self.ability, "modifier_item_charik_new_regen", {})
 	end
 
--- game/overvodka/scripts/vscripts/items/item_charik_new.lua
+++ game/overvodka/scripts/vscripts/items/item_charik_new.lua
@@ -16,8 +16,23 @@ function modifier_item_charik_new:GetAttributes()  return MODIFIER_ATTRIBUTE_MUL
 
 function modifier_item_charik_new:OnCreated()
 	if not IsServer() then return end
-	if not self:GetCaster():HasModifier("modifier_item_charik_new_regen") then
-		self:GetCaster():AddNewModifier(self:GetCaster(), self:GetAbility(), "modifier_item_charik_new_regen", {})
+
+	local parent = self:GetCaster()
+	local ability = self:GetAbility()
+	if not parent or parent:IsNull() then return end
+	if not ability or ability:IsNull() then return end
+
+	local regen = parent:FindModifierByName("modifier_item_charik_new_regen")
+	if regen and not regen:IsNull() then
+		local regen_ability = regen:GetAbility()
+		if not regen_ability or regen_ability:IsNull() then
+			regen:Destroy()
+			regen = nil
+		end
+	end
+
+	if not regen then
+		parent:AddNewModifier(parent, ability, "modifier_item_charik_new_regen", {})
 	end
 end
 
